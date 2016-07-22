de03e5f515934e1a441e84d06b29129fa9bc4f22
Vincent Chase <chiwenzhen@gmail.com>
2016-07-22 17:14:45 +0800

    改用滑条输入数据，而非输入框

--- main.py
+++ main.py
@@ -5,15 +5,22 @@ chiwenzhen
 @ruijie
 
 """
-
-import matplotlib
+import numpy as np
 from Tkinter import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from model import *
 
+
 class App:
     def __init__(self):
+        # 分类器训练
+        self.evaluator = BreastCancerEvaluator()
+        self.evaluator.load_data()
+        self.evaluator.train()
+        x_origin, y = self.evaluator.get_train_data()
+        x = self.evaluator.reduce(x_origin)
+
         # 构建UI
         self.root = Tk()
         self.root.wm_title("Breast Cancer Evaluation Platform")
@@ -27,82 +34,73 @@ class App:
         canvas = FigureCanvasTkAgg(self.figure, master=self.root)
         canvas.show()
         canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
-        canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=1)
+        canvas.tkcanvas.pack(side=TOP, fill=BOTH, expand=1)
 
         # 添加两个输入框，用于输入细胞特征
-        self.V1 = StringVar()
-        self.V1.set("18.0")
-        L1 = Label(self.root, text="radius")
-        L1.pack(side=LEFT)
-        E1 = Entry(self.root, textvariable=self.V1, bd=5)
-        E1.pack(side=LEFT)
+        label1 = Label(self.root, text="radius")
+        label1.pack(side=LEFT)
+        self.slide1 = Scale(self.root, from_=np.min(x_origin[:, 0]), to=np.max(x_origin[:, 0]), orient=HORIZONTAL,
+                            command=self.predict)
+        self.slide1.pack(side=LEFT)
 
-        self.V2 = StringVar()
-        self.V2.set("10.0")
-        L2 = Label(self.root, text="texture")
-        L2.pack(side=LEFT)
-        E2 = Entry(self.root, textvariable=self.V2, bd=5)
-        E2.pack(side=LEFT)
+        label2 = Label(self.root, text="texture")
+        label2.pack(side=LEFT)
+        self.slide2 = Scale(self.root, from_=np.min(x_origin[:, 1]), to=np.max(x_origin[:, 1]), orient=HORIZONTAL,
+                            command=self.predict)
+        self.slide2.pack(side=LEFT)
 
-        self.V3 = StringVar()
-        L3 = Label(self.root, text="malignant prob")
-        L3.pack(side=LEFT)
-        self.E3 = Entry(self.root, textvariable=self.V3, bd=5)
-        self.E3.pack(side=LEFT)
-
-        # 分类器训练
-        self.evaluator = BreastCancerEvaluator()
-        self.evaluator.load_data()
-        self.evaluator.train()
-        X, y = self.evaluator.get_train_data_r()
+        self.malig_prob = StringVar()
+        label3 = Label(self.root, text="malignant prob")
+        label3.pack(side=LEFT)
+        self.entry3 = Entry(self.root, textvariable=self.malig_prob, bd=5)
+        self.entry3.pack(side=LEFT)
 
         # matplotlib绘制图
         self.subplot = self.figure.add_subplot(111)
-        self.plot_subplot(self.subplot, X, y)
-        minx = np.min(X[:, 0])
-        maxx = np.max(X[:, 0])
-        self.plot_hyperplane(self.subplot, self.evaluator.get_clf(), minx, maxx)
+        self.plot_subplot(self.subplot, x, y)
+        minx = np.min(x[:, 0])
+        maxx = np.max(x[:, 0])
+        plot_hyperplane(self.subplot, self.evaluator.get_clf(), minx, maxx)
         self.last_line = None
 
-        # 添加一个按钮，用于评估具体案例中肿瘤属于良性/恶性
-        button = Button(master=self.root, text='Predict', command=self.predict)
-        button.pack(side=BOTTOM)
-
-    def plot_hyperplane(self, subplot, clf, min_x, max_x):
-        # get the separating hyperplane
-        w = clf.coef_[0]
-        a = -w[0] / w[1]
-        xx = np.linspace(min_x, max_x)
-        yy = a * xx - (clf.intercept_[0]) / w[1]
-        subplot.plot(xx, yy, "black")
-
-    def plot_subplot(self, subplot, X, y):
-        X_zero = X[y == 0]
-        X_one = X[y == 1]
-        subplot.plot(X_zero[:, 0], X_zero[:, 1], "g.", label="benign")
-        subplot.plot(X_one[:, 0], X_one[:, 1], "k.", label="malignant")
+    @staticmethod
+    def plot_subplot(subplot, x, y):
+        x_zero = x[y == 0]
+        x = x[y == 1]
+        subplot.plot(x_zero[:, 0], x_zero[:, 1], "g.", label="benign")
+        subplot.plot(x[:, 0], x[:, 1], "k.", label="malignant")
         subplot.set_title('Model Illustration')
         subplot.set_xlabel('x1')
         subplot.set_ylabel('x2')
 
-    def plot_point(self, subplot, X):
-        if self.last_line != None:
+    def plot_point(self, subplot, x):
+        if self.last_line is not None:
             self.last_line.remove()
             del self.last_line
-        lines = subplot.plot(X[:, 0], X[:, 1], "ro", label="case")
+        lines = subplot.plot(x[:, 0], x[:, 1], "ro", label="case")
         self.last_line = lines.pop(0)
 
-    def predict(self):
-        X = np.array([[float(self.V1.get()),float(self.V2.get()),122.8,1001,0.1184,0.2776,0.3001,0.1471,0.2419,0.07871,1.095,0.9053,8.589,153.4,0.006399,0.04904,0.05373,0.01587,0.03003,0.006193,25.38,17.33,184.6,2019,0.1622,0.6656,0.7119,0.2654,0.4601,0.1189]])
-        result, newX = self.evaluator.predict(X)
-        self.V3.set(result[0, 1]) # 恶性肿瘤的概率
-        self.plot_point(self.subplot, self.evaluator.reduce(X))
+    def predict(self, trival):
+        x = np.array([[float(self.slide1.get()), float(self.slide2.get()), 122.8, 1001, 0.1184, 0.2776, 0.3001, 0.1471,
+                       0.2419, 0.07871, 1.095, 0.9053, 8.589, 153.4, 0.006399, 0.04904, 0.05373, 0.01587, 0.03003,
+                       0.006193, 25.38, 17.33, 184.6, 2019, 0.1622, 0.6656, 0.7119, 0.2654, 0.4601, 0.1189]])
+        result = self.evaluator.predict(x)
+        self.malig_prob.set(result[0, 1])  # 恶性肿瘤的概率
+        self.plot_point(self.subplot, self.evaluator.reduce(x))
         self.figure.canvas.draw()
 
     def run(self):
         self.root.mainloop()
 
+
+def plot_hyperplane(subplot, clf, min_x, max_x):
+    # get the separating hyperplane
+    w = clf.coef_[0]
+    xx = np.linspace(min_x, max_x)
+    yy = -(w[0] * xx + clf.intercept_[0]) / w[1]
+    subplot.plot(xx, yy, "black")
+
+
 if __name__ == "__main__":
     app = App()
     app.run()
-
--- model.py
+++ model.py
@@ -14,20 +14,27 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder
 from sklearn.cross_validation import train_test_split
 from sklearn.externals import joblib
-import matplotlib.pyplot as plt
-import numpy as np
 
 
 class BreastCancerEvaluator:
+    def __init__(self):
+        self.x_train = None
+        self.x_test = None
+        self.y_train = None
+        self.y_test = None
+        self.scaler = None
+        self.pca = None
+        self.clf = None
+        self.pipeline = None
 
     # 载入数据集
-    def load_data(self, train_data_path ="wdbc.data"):
+    def load_data(self, train_data_path="wdbc.data"):
         df = pd.read_csv(train_data_path, header=None)
-        X = df.loc[:, 2:].values  # 训练集特征
+        x = df.loc[:, 2:].values  # 训练集特征
         y = df.loc[:, 1].values  # 训练集标签
         le = LabelEncoder()
         y = le.fit_transform(y)  # 把字符串标签转换为整数，恶性M-1，良性B-0
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.20,
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x, y, test_size=0.20,
                                                                                 random_state=1)  # 拆分成训练集(80%)和测试集(20%)
 
     # 训练模型
@@ -37,40 +44,38 @@ class BreastCancerEvaluator:
         self.clf = LogisticRegression(random_state=1)
         estimators = [('scl', self.scaler), ('pca', self.pca), ('clf', self.clf)]
         self.pipeline = Pipeline(estimators)  # 可以通过pipe_lr.named_steps['pca']来访问PCA对象
-        self.pipeline.fit(self.X_train, self.y_train)
+        self.pipeline.fit(self.x_train, self.y_train)
 
     # 评估模型
     def score(self):
-        return self.pipeline.score(self.X_test, self.y_test)
+        return self.pipeline.score(self.x_test, self.y_test)
 
     # 保存模型
     def dump_model(self):
         joblib.dump(self.pipeline, "train.model")
 
-    # 获取降维后的数据
-    def get_train_data_r(self):
-        X_train_decomp = self.scaler.transform(self.X_train)
-        X_train_decomp = self.pca.transform(X_train_decomp)
-        return X_train_decomp, self.y_train
-
-    # 获取训练后的分类器参数
+    # 获取分类器
     def get_clf(self):
         return self.clf
 
     # 輸入原始特征進行分類
-    def predict(self, X):
-        X = self.scaler.transform(X)
-        X = self.pca.transform(X)
-        return self.clf.predict_proba(X), X
+    def predict(self, x):
+        x = self.scaler.transform(x)
+        x = self.pca.transform(x)
+        return self.clf.predict_proba(x)
 
     # 输入降维后特征进行分类
-    def predict_reduced(self, X):
-        X = self.scaler.transform(X)
-        X = self.pca.transform(X)
-        return self.clf.predict_proba(X), X
+    def predict_r(self, x):
+        x = self.scaler.transform(x)
+        x = self.pca.transform(x)
+        return self.clf.predict_proba(x), x
 
     # 特征降维
-    def reduce(self, X):
-        X = self.scaler.transform(X)
-        X = self.pca.transform(X)
-        return X
+    def reduce(self, x):
+        x = self.scaler.transform(x)
+        x = self.pca.transform(x)
+        return x
+
+    # 获取训练数据
+    def get_train_data(self):
+        return self.x_train, self.y_train
