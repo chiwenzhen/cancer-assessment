98249e05715d44e58e6687b901c7226774dbd370
Vincent Chase <chiwenzhen@gmail.com>
2016-08-02 16:10:42 +0800

    采用多项式核的SVM

--- model.py
+++ model.py
@@ -14,6 +14,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder
 from sklearn.cross_validation import train_test_split
 from sklearn.externals import joblib
+from sklearn.svm import SVC
 
 
 class CancerEvaluator:
@@ -25,7 +26,7 @@ class CancerEvaluator:
         self.scaler = StandardScaler()
         self.pca = PCA(n_components=2)
         self.clf = LogisticRegression(penalty='l2', random_state=1)
-        # self.clf = SVC(kernel='linear', probability=True, random_state=1)
+        # self.clf = SVC(kernel='poly', degree=2, probability=True, random_state=1)
         self.estimators = [('scl', self.scaler), ('pca', self.pca), ('clf', self.clf)]
         self.pipeline = Pipeline(self.estimators)  # 可以通过pipe.named_steps['pca']来访问PCA对象
 
